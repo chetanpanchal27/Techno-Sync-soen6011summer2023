e43cdea49c8ac49c4114df6035a64354bafc97fa
urvishkapadiya <kapadiyaurvish16@gmail.com>
2023-07-17 18:44:16 -0400

    US-6 Add feature to post a job (User: Recruiter)

---
+++ backend/routes/apiRoutes.js
@@ -0,0 +1,477 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const jwtAuth = require("../lib/jwtAuthentication");
+
+const User = require("../db/User");
+const JobApplicant = require("../db/Candidate");
+const Recruiter = require("../db/Employer");
+const Job = require("../db/Job");
+const Application = require("../db/App");
+
+//The express.Router() function is used to create a new router object.
+//This function is used when you want to create a new router object in your program to handle requests.
+const router = express.Router();
+
+// To add new job
+router.post("/jobs", jwtAuth, (req, res) => {
+  const user = req.user;
+
+  if (user.type != "recruiter") {
+    // applicant trying to add new job
+    // not authorised
+    res.status(401).json({
+      message: "You don't have permissions to add jobs",
+    });
+    return;
+  }
+
+  const data = req.body;
+
+  let job = new Job({
+    userId: user._id,
+    title: data.title,
+    maxApplicants: data.maxApplicants,
+    maxPositions: data.maxPositions,
+    dateOfPosting: data.dateOfPosting,
+    deadline: data.deadline,
+    skillsets: data.skillsets,
+    jobType: data.jobType,
+    duration: data.duration,
+    salary: data.salary,
+    rating: data.rating,
+  });
+
+  job
+    .save()
+    .then(() => {
+      res.json({ message: "Job added successfully to the database" });
+    })
+    .catch((err) => {
+      //Whenever any user sends an invalid request to the server,
+      //the server immediately reports it and generates an HTTP based 400 bad request error.
+      res.status(400).json(err);
+    });
+});
+
+// to get all the jobs [pagination] [for recruiter personal and for everyone]
+router.get("/jobs", jwtAuth, (req, res) => {
+  console.log("Umang")
+  let user = req.user;
+
+  let findParams = {};
+  let sortParams = {};
+
+  // to list down jobs posted by a particular recruiter
+  console.log("-->>",user,"--->",req.query.myjobs);
+  if (user.type === "recruiter" && req.query.myjobs) {
+    findParams = {
+      ...findParams,
+      userId: user._id,
+    };
+  }
+
+  if (req.query.q) {
+    findParams = {
+      ...findParams,
+      title: {
+        $regex: new RegExp(req.query.q, "i"),
+      },
+    };
+  }
+
+  if (req.query.jobType) {
+    let jobTypes = [];
+    if (Array.isArray(req.query.jobType)) {
+      jobTypes = req.query.jobType;
+    } else {
+      jobTypes = [req.query.jobType];
+    }
+    console.log(jobTypes);
+    findParams = {
+      ...findParams,
+      jobType: {
+        $in: jobTypes,
+      },
+    };
+  }
+
+  if (req.query.salaryMin && req.query.salaryMax) {
+    findParams = {
+      ...findParams,
+      $and: [
+        {
+          salary: {
+            $gte: parseInt(req.query.salaryMin),
+          },
+        },
+        {
+          salary: {
+            $lte: parseInt(req.query.salaryMax),
+          },
+        },
+      ],
+    };
+  } else if (req.query.salaryMin) {
+    findParams = {
+      ...findParams,
+      salary: {
+        $gte: parseInt(req.query.salaryMin),
+      },
+    };
+  } else if (req.query.salaryMax) {
+    findParams = {
+      ...findParams,
+      salary: {
+        $lte: parseInt(req.query.salaryMax),
+      },
+    };
+  }
+
+  if (req.query.duration) {
+    findParams = {
+      ...findParams,
+      duration: {
+        $lt: parseInt(req.query.duration),
+      },
+    };
+  }
+
+  if (req.query.asc) {
+    if (Array.isArray(req.query.asc)) {
+      req.query.asc.map((key) => {
+        sortParams = {
+          ...sortParams,
+          [key]: 1,
+        };
+      });
+    } else {
+      sortParams = {
+        ...sortParams,
+        [req.query.asc]: 1,
+      };
+    }
+  }
+
+  if (req.query.desc) {
+    if (Array.isArray(req.query.desc)) {
+      req.query.desc.map((key) => {
+        sortParams = {
+          ...sortParams,
+          [key]: -1,
+        };
+      });
+    } else {
+      sortParams = {
+        ...sortParams,
+        [req.query.desc]: -1,
+      };
+    }
+  }
+
+  console.log(findParams);
+  console.log(sortParams);
+
+  // Job.find(findParams).collation({ locale: "en" }).sort(sortParams);
+  // .skip(skip)
+  // .limit(limit)
+
+  let arr = [
+    {
+      $lookup: {
+        from: "recruiterinfos",
+        localField: "userId",
+        foreignField: "userId",
+        as: "recruiter",
+      },
+    },
+    { $unwind: "$recruiter" },
+    { $match: findParams },
+  ];
+
+  if (Object.keys(sortParams).length > 0) {
+    arr = [
+      {
+        $lookup: {
+          from: "recruiterinfos",
+          localField: "userId",
+          foreignField: "userId",
+          as: "recruiter",
+        },
+      },
+      { $unwind: "$recruiter" },
+      { $match: findParams },
+      {
+        $sort: sortParams,
+      },
+    ];
+  }
+
+  console.log(arr);
+
+  Job.aggregate(arr)
+    .then((posts) => {
+      if (posts == null) {
+        res.status(404).json({
+          message: "No job found",
+        });
+        return;
+      }
+      res.json(posts);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+// to get info about a particular job
+router.get("/jobs/:id", jwtAuth, (req, res) => {
+  Job.findOne({ _id: req.params.id })
+    .then((job) => {
+      if (job == null) {
+        res.status(400).json({
+          message: "Job does not exist",
+        });
+        return;
+      }
+      res.json(job);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+// to update info of a particular job
+router.put("/jobs/:id", jwtAuth, (req, res) => {
+  const user = req.user;
+  if (user.type != "recruiter") {
+    // 401- unauthorised
+    res.status(401).json({
+      message: "You don't have permissions to change the job details",
+    });
+    return;
+  }
+  Job.findOne({
+    _id: req.params.id,
+    userId: user.id,
+  })
+    .then((job) => {
+      if (job == null) {
+        // 404 - not found
+        res.status(404).json({
+          message: "Job does not exist",
+        });
+        return;
+      }
+      const data = req.body;
+      if (data.maxApplicants) {
+        job.maxApplicants = data.maxApplicants;
+      }
+      if (data.maxPositions) {
+        job.maxPositions = data.maxPositions;
+      }
+      if (data.deadline) {
+        job.deadline = data.deadline;
+      }
+      job
+        .save()
+        .then(() => {
+          res.json({
+            message: "Job details updated successfully",
+          });
+        })
+        .catch((err) => {
+          res.status(400).json(err);
+        });
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+// to delete a job
+router.delete("/jobs/:id", jwtAuth, (req, res) => {
+  const user = req.user;
+  if (user.type != "recruiter") {
+    res.status(401).json({
+      message: "You don't have permissions to delete the job",
+    });
+    return;
+  }
+  Job.findOneAndDelete({
+    _id: req.params.id,
+    userId: user.id,
+  })
+    .then((job) => {
+      if (job === null) {
+        res.status(401).json({
+          message: "You don't have permissions to delete the job",
+        });
+        return;
+      }
+      res.json({
+        message: "Job deleted successfully",
+      });
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+// get user's personal details
+router.get("/user", jwtAuth, (req, res) => {
+  const user = req.user;
+  if (user.type === "recruiter") {
+    Recruiter.findOne({ userId: user._id })
+      .then((recruiter) => {
+        if (recruiter == null) {
+          res.status(404).json({
+            message: "User does not exist",
+          });
+          return;
+        }
+        res.json(recruiter);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  } else {
+    JobApplicant.findOne({ userId: user._id })
+      .then((jobApplicant) => {
+        if (jobApplicant == null) {
+          res.status(404).json({
+            message: "User does not exist",
+          });
+          return;
+        }
+        res.json(jobApplicant);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  }
+});
+
+// get user details from id
+router.get("/user/:id", jwtAuth, (req, res) => {
+  User.findOne({ _id: req.params.id })
+    .then((userData) => {
+      if (userData === null) {
+        res.status(404).json({
+          message: "User does not exist",
+        });
+        return;
+      }
+
+      if (userData.type === "recruiter") {
+        Recruiter.findOne({ userId: userData._id })
+          .then((recruiter) => {
+            if (recruiter === null) {
+              res.status(404).json({
+                message: "User does not exist",
+              });
+              return;
+            }
+            res.json(recruiter);
+          })
+          .catch((err) => {
+            res.status(400).json(err);
+          });
+      } else {
+        JobApplicant.findOne({ userId: userData._id })
+          .then((jobApplicant) => {
+            if (jobApplicant === null) {
+              res.status(404).json({
+                message: "User does not exist",
+              });
+              return;
+            }
+            res.json(jobApplicant);
+          })
+          .catch((err) => {
+            res.status(400).json(err);
+          });
+      }
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+// update user details
+router.put("/user", jwtAuth, (req, res) => {
+  const user = req.user;
+  const data = req.body;
+  if (user.type == "recruiter") {
+    Recruiter.findOne({ userId: user._id })
+      .then((recruiter) => {
+        if (recruiter == null) {
+          res.status(404).json({
+            message: "User does not exist",
+          });
+          return;
+        }
+        if (data.name) {
+          recruiter.name = data.name;
+        }
+        if (data.contactNumber) {
+          recruiter.contactNumber = data.contactNumber;
+        }
+        if (data.bio) {
+          recruiter.bio = data.bio;
+        }
+        recruiter
+          .save()
+          .then(() => {
+            res.json({
+              message: "User information updated successfully",
+            });
+          })
+          .catch((err) => {
+            res.status(400).json(err);
+          });
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  } else {
+    JobApplicant.findOne({ userId: user._id })
+      .then((jobApplicant) => {
+        if (jobApplicant == null) {
+          res.status(404).json({
+            message: "User does not exist",
+          });
+          return;
+        }
+        if (data.name) {
+          jobApplicant.name = data.name;
+        }
+        if (data.education) {
+          jobApplicant.education = data.education;
+        }
+        if (data.skills) {
+          jobApplicant.skills = data.skills;
+        }
+        if (data.resume) {
+          jobApplicant.resume = data.resume;
+        }
+        if (data.profile) {
+          jobApplicant.profile = data.profile;
+        }
+        console.log(jobApplicant);
+        jobApplicant
+          .save()
+          .then(() => {
+            res.json({
+              message: "User information updated successfully",
+            });
+          })
+          .catch((err) => {
+            res.status(400).json(err);
+          });
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  }
+});
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,13 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true}));
-app.use(passportConfig.initialize());
-
 app.use(cors());
 app.use(express.json());
+app.use(passportConfig.initialize());
+
 
 app.listen(port, () =>{
     console.log("Server is listening on port "+port)
 })
 app.use("/auth", require("./routes/authRoutes"));
+app.use("/api", require("./routes/apiRoutes"));
