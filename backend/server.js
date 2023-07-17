60d9b9e53fae8b961ad0c2e07cfebbf74850a604
Dvij <dvijbarot17@gmail.com>
2023-07-17 16:53:55 -0400

    New branch Dvij

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mongoose =  require("mongoose");
+const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
 const passportConfig = require("./lib/passportConfig");
 var cors = require("cors");
@@ -7,16 +7,16 @@ var cors = require("cors");
 require("./db/connect");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: true}));
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(passportConfig.initialize());
 
 app.use(cors());
 app.use(express.json());
 
-app.listen(port, () =>{
-    console.log("Server is listening on port "+port)
-})
+app.listen(port, () => {
+  console.log("Server is listening on port " + port);
+});
 app.use("/auth", require("./routes/authRoutes"));
