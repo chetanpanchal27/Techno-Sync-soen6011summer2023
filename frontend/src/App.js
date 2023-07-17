195072c4bb676bd5e77a8b4dba43808e2fc903d6
Chetan Panchal <61734206+chetanpanchal27@users.noreply.github.com>
2023-07-17 14:14:46 -0400

    Revert "Umang"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,10 @@
-import { createContext, useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from "./views/Login";
-import MessagePopupBox from "./Helper/MessagePopupBox";
-import { Grid, makeStyles } from "@material-ui/core";
-import SignUpPage from "./views/Singup";
 
-const useStyles = makeStyles((theme) => ({
-  body: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    minHeight: "98vh",
-    paddingTop: "64px",
-    boxSizing: "border-box",
-    width: "100%",
-  },
-}));
 
-export const PopupContext = createContext();
 function App() {
-  const styles = useStyles();
-  const [popup, setPopup] = useState({
-    isOpen: false,
-    severity: "",
-    message: "",
-  });
-
   return (
-    <BrowserRouter>
-      <PopupContext.Provider value={setPopup}>
-        <Grid container direction="column">
-          <Grid item></Grid>
-          <Grid item className={styles.body}>
-            <Routes>
-              <Route exact path="/" element={<LoginPage />}></Route>
-              <Route path="/login" element={<LoginPage />}></Route>
-              <Route path="/login" element={<SignUpPage />}></Route>
-            </Routes>
-          </Grid>
-        </Grid>
-      </PopupContext.Provider>
-      <MessagePopupBox
-        open={popup.isOpen}
-        setOpen={(status) =>
-          setPopup({
-            ...popup,
-            isOpen: status,
-          })
-        }
-        severity={popup.severity}
-        message={popup.message}
-      />
-    </BrowserRouter>
+    <div>
+     <p>Welcome to JOB STATION</p>
+    </div>
   );
 }
 
